b50f0f3cd8214d68e6497e1dcfc73aec520ae223
Hartanto Ario Widjaya <tanto259@users.noreply.github.com>
2020-10-03 21:25:56 +0800

    Initial commit on GitHub

---
+++ README.md
@@ -0,0 +1,8 @@
+# CS 203 G1T7 Backend API
+
+Team members:
+ - Charlie
+ - Cheyenne
+ - Graciella
+ - Hartanto
+ - Megan
---
+++ src/main/java/cs203/g1t7/ryverbank/RyverBankApplication.java
@@ -0,0 +1,13 @@
+package cs203.g1t7.ryverbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RyverBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RyverBankApplication.class, args);
+	}
+
+}
