9d384696b09e4d6d6b4cf0f1214d14aa2376df07
charlieangriawan <charlieangriawan@hotmail.com>
2020-10-29 23:31:12 +0800

    Added quote from Yahoo finance api (via rapid api)

--- src/main/java/cs203/g1t7/asset/Quote.java
+++ src/main/java/cs203/g1t7/asset/Quote.java
@@ -16,6 +16,8 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import lombok.*;
 
+import yahoofinance.Stock;
+import yahoofinance.YahooFinance;
 
 @Entity
 @Getter
@@ -50,4 +52,27 @@ public class Quote {
     @Column (name = "ask")
     private Double ask;
     
+    // public Quote() {
+    //     try {
+    //         String[] symbols = new String[] { "A17U", "C61U", "C31", "C38U", "C09", "C52", "D01", "D05", "G13", "H78",
+    //             "C07", "J36", "J37", "BN4", "N2IU", "ME8U", "M44U", "O39", "S58", "U96", "S68", "C6L", "Z74", "S63",
+    //             "Y92", "U11", "U14", "V03", "F34", "BS6" };
+
+    //         for (String symbol : symbols) {
+    //             Stock stock = YahooFinance.get(symbol + ".SI");
+    //             // double bid = stock.getQuote().getBid().doubleValue();
+    //             double bid = 0.0;
+    //             double price = stock.getQuote().getPrice().doubleValue();
+    //             // double ask = stock.getQuote().getAsk().doubleValue();
+    //             double ask = 0.0;
+    //             int bidVolume = 0;
+    //             // stock.getQuote().getBidSize().intValue();
+    //             int askVolume = 0;
+    //             // stock.getQuote().getAskSize().intValue();
+    //             stockRepository.save(new CustomStock(symbol, price, bidVolume, bid, askVolume, ask));
+    //         }
+    //     } catch (IOException e) {
+    //         System.out.println("One of the stock is not found");
+    //     }
+    // }
 }
--- src/main/java/cs203/g1t7/asset/QuoteController.java
+++ src/main/java/cs203/g1t7/asset/QuoteController.java
@@ -11,8 +11,20 @@ import org.springframework.http.HttpStatus;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.client.RestTemplate;
 
+import org.json.JSONObject;
+import org.json.JSONArray;
+import org.json.JSONException;
+
 import cs203.g1t7.asset.*;
-// import cs203.g1t7.users.*;
+
+// import yahoofinance.Stock;
+// import yahoofinance.YahooFinance;
+// import java.io.IOException;
+
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.RequestBody;
+import com.squareup.okhttp.Response;// import cs203.g1t7.users.*;
 
 @RestController
 public class QuoteController {
@@ -31,25 +43,48 @@ public class QuoteController {
     @GetMapping("/api/stocks/{asset_id}")
     public Quote getQuote(@PathVariable String asset_id)
     {
-        final String uri = String.format("https://finnhub.io/api/v1/stock/price-target?symbol=%s&token=bu3iu1v48v6up0bhtaeg", asset_id);
-    
-        RestTemplate restTemplate = new RestTemplate();
-        String result = restTemplate.getForObject(uri, String.class);
-        // {"lastUpdated":"","symbol":"","targetHigh":0,"targetLow":0,"targetMean":0,"targetMedian":0}
-        String[] split = result.split("\"");
+        OkHttpClient client = new OkHttpClient();
+        try {
+            Request request = new Request.Builder()
+                .url("https://apidojo-yahoo-finance-v1.p.rapidapi.com/market/v2/get-quotes?symbols=A17U.SI&region=US")
+                .get()
+                .addHeader("x-rapidapi-host", "apidojo-yahoo-finance-v1.p.rapidapi.com")
+                .addHeader("x-rapidapi-key", "75bb23538fmsh1a3ba597c8a23f7p1dbc62jsn3af1f51b6efe")
+                .build();
         
-        String sym = split[7];
-        Double price = Double.parseDouble(split[14].substring(1, split[14].length() - 2));
+            Response response = client.newCall(request).execute();
 
-        Quote quote;
-        if (quotes.findBySymbol(sym) != null) {
-            quote = quotes.findBySymbol(sym);
-            quote.setLast_price(price);
-            quote.setBid(price - 0.01);
-            quote.setAsk(price + 0.01);
-        } else {
-            quote = new Quote(sym, price, 20000, price - 0.01, 20000, price + 0.01);
+            String jsonData = response.body().string();
+            try {
+                JSONObject Jobject = new JSONObject(jsonData);
+                JSONObject quoteResponse = Jobject.getJSONObject("quoteResponse");
+                JSONArray result = quoteResponse.getJSONArray("result");
+                JSONObject stock = result.getJSONObject(0);
+                
+                Double price = stock.getDouble("regularMarketPrice");
+                Double ask = stock.getDouble("ask");
+                Double bid = stock.getDouble("bid");
+                String askVol = stock.getString("askSize");
+                String bidVol = stock.getString("bidSize");
+                
+                Integer askVolume = Integer.parseInt(askVol);
+                Integer bidVolume = Integer.parseInt(bidVol);
+
+                Quote quote;
+                if (quotes.findBySymbol(asset_id) != null) {
+                    quote = quotes.findBySymbol(asset_id);
+                    quote.setLast_price(price);
+                    quote.setBid(bid);
+                    quote.setAsk(ask);
+                } else {
+                    quote = new Quote(asset_id, price, bidVolume, bid, askVolume, ask);
+                }
+                return quotes.save(quote);
+            } catch (JSONException e) {
+                throw new QuoteNotFoundException(asset_id);
+            }
+        } catch (IOException e) {
+            throw new QuoteNotFoundException(asset_id);
         }
-        return quotes.save(quote);
     }
 }
