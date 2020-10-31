27d7915ad92a78f51d8c01bff8ba6e7514d038d1
Graciella Antonius Putri <61402880+venushmallow@users.noreply.github.com>
2020-10-31 15:42:27 +0800

    Add a new constructor for 2 params

--- src/main/java/cs203/g1t7/account/Account.java
+++ src/main/java/cs203/g1t7/account/Account.java
@@ -36,6 +36,12 @@ public class Account {
     @OneToMany(mappedBy = "account", cascade = CascadeType.ALL, orphanRemoval = true)
     @JsonIgnore
     private List<Transaction> transactions;
+    
+    public Account(Integer customer_id, double balance){
+        this.customer_id = customer_id;
+        this.balance = balance;
+        this.available_balance = balance;
+    }
 
     public Account(Integer customer_id, double balance, double available_balance){
         this.customer_id = customer_id;
